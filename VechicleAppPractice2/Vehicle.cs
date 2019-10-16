5adf3e80ce362319e6a093c13d900b5a3b806828
sfarhana <fsultanacse@gmail.com>
2019-10-16 23:55:51 +0600

    Assignment 10 added

---
+++ VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    class Vehicle
+    {
+        private List<double> speed = new List<double>();
+        private string vehicleName;
+        private string regNo;
+
+        public string VehicleName { set; get; }
+        public string RegNo { set; get; }
+
+        public void SetList(double value)
+        {
+            speed.Add(value);
+        }
+
+        private double minSpeed;
+        public double MinSpeed
+        {
+            get
+            {
+                return this.minSpeed = speed.Min();
+            }
+        }
+
+        private double maxSpeed;
+        public double MaxSpeed
+        {
+            get
+            {
+                return this.maxSpeed = speed.Max();
+            }
+        }
+
+        private double avgSpeed;
+        public double AvgSpeed
+        {
+            get
+            {
+                return this.avgSpeed = speed.Average();
+            }
+        }
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        Vehicle vh = new Vehicle();
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            vh.VehicleName = vehicleNameTextBox.Text;
+            vh.RegNo = regNoTextBox.Text;
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            vh.SetList(Convert.ToDouble(speedTextBox.Text));
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = vh.MinSpeed.ToString();
+            maxSpeedTextBox.Text = vh.MaxSpeed.ToString();
+            averageSpeedTextBox.Text = Math.Round(vh.AvgSpeed,3).ToString();
+        }
+    }
+}
